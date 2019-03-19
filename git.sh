994896dffa31258dbd5ae4d099c8be3102bb04be
praveennagulapally <48675366+praveennagulapally@users.noreply.github.com>
2019-03-19 11:26:04 +0530

    git.sh

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# PravRep" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/praveennagulapally/PravRep.git
+git push -u origin master
